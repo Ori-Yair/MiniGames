79fa789f106eeb3faee6a69d332efea9d956d0fc
Ori Yair <oriyair14@gmail.com>
2021-04-01 04:14:37 +0300

    Created walls in Snake

--- Assets/Scripts/Snake/SnakePlayerController.cs
+++ Assets/Scripts/Snake/SnakePlayerController.cs
@@ -14,21 +14,21 @@ public class SnakePlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("up"))
-        {
-            snakeMove.ChangeDirection(SnakeMove.Direction.Up);
-        }
-        if (Input.GetKey("down"))
-        {
-            snakeMove.ChangeDirection(SnakeMove.Direction.Down);
-        }
-        if (Input.GetKey("right"))
-        {
-            snakeMove.ChangeDirection(SnakeMove.Direction.Right);
-        }
         if (Input.GetKey("left"))
         {
             snakeMove.ChangeDirection(SnakeMove.Direction.Left);
+        }
+        else if (Input.GetKey("down"))
+        {
+            snakeMove.ChangeDirection(SnakeMove.Direction.Down);
+        }
+        else if(Input.GetKey("right"))
+        {
+            snakeMove.ChangeDirection(SnakeMove.Direction.Right);
+        }
+        else if(Input.GetKey("up"))
+        {
+            snakeMove.ChangeDirection(SnakeMove.Direction.Up);
         }
     }
 }
